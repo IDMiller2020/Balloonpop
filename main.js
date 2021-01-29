5b84cca1398754160b2c799ecdbe0de08e8175d1
IDMiller2020 <psiauto@gmail.com>
2021-01-28 17:03:43 -0700

    Added countdown timer

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
 let startButton = document.getElementById("start-button")
 let inflateButton = document.getElementById("inflate-button")
+
+// DATA
 let clickCount = 0
 let height = 120
 let width = 100
 let inflationRate = 20
 let maxsize = 300
 let popCount = 0
+let gameLenght = 5000
+let clockId = 0
+let timeRemaining = 0
 
 
 function startGame(){
 
   startButton.setAttribute("disabled", "true")
   inflateButton.removeAttribute("disabled")
+  startClock()
+  setTimeout(stopGame, gameLenght)
+}
 
-  setTimeout(stopGame, 3000)
+function startClock(){
+  timeRemaining = gameLenght
+  drawClock()
+  clockId = setInterval(drawClock, 1000)
+}
+
+function stopClock(){
+  clearInterval(clockId)
+}
+
+function drawClock(){
+  let countdownElem = document.getElementById("countdown")
+  countdownElem.innerText = (timeRemaining / 1000).toString()
+  timeRemaining -= 1000
 }
 
 function inflate(){
@@ -53,5 +74,6 @@ function stopGame(){
   height = 120
   width = 100
 
+  stopClock()
   draw()
 }
